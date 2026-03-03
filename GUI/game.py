3a9836540c02826b3753fc11fd73a382aa460380
The Lake laptop20 <109922689+Karolon@users.noreply.github.com>
2026-03-03 16:26:11 +0100

    lekcja

--- GUI/game.py
+++ GUI/game.py
@@ -1,16 +1,15 @@
 import tkinter as tk
 
-
 class Buildings:
     def __init__(self):
-        self.buildingCount = 0
         self.cursor_x, self.cursor_y = 0, 0 
         self.windowList = []
         temp =tk.Tk()
         temp.withdraw()
         self.screen_width = temp.winfo_screenwidth()
         self.screen_height = temp.winfo_screenheight()
-        temp.destroy() 
+        temp.destroy()
+        self.house_image = tk.PhotoImage(file = "tall_house.png")
         
         
         
@@ -20,7 +19,9 @@ class Buildings:
             print('placed')
 
         def move(a):
-            house_win.geometry(f'150x400+{min(max(a.x_root-self.cursor_x-8, -8), self.screen_width-house_win.winfo_width())}+{min(max(a.y_root-self.cursor_y-31, 0), self.screen_height-house_win.winfo_height())}')
+            taskBarSize = house_win.winfo_height() - house_win.winfo_y()
+            print(taskBarSize)
+            house_win.geometry(f'132x279+{min(max(a.x_root-self.cursor_x-8, -8), self.screen_width-house_win.winfo_width())}+{min(max(a.y_root-self.cursor_y-31, 0), self.screen_height-house_win.winfo_height())}')
             
         def start_moving(a):
             self.cursor_x  = a.x
@@ -31,10 +32,13 @@ class Buildings:
                 placeBuilding()
 
         house_win=tk.Toplevel(root)
-        house_win.geometry("150x400")
+        house_win.geometry("132x279")
         house_win.title("House")
         house_win.resizable(False, False)
-        self.buildingCount += 1
+        background_label = tk.Label(house_win, image = self.house_image, bg="white")
+        background_label.place(x = 0, y = 0)
+        house_win.wm_attributes("-transparentcolor", "white")
+        house_win.wm_attributes()
         house_win.bind("<Button-1>", start_moving)
         house_win.bind("<B1-Motion>", move)
         house_win.bind("<B1-ButtonRelease>", stop_moving)
